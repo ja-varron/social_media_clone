9d79d5e0005bbb46842ae403c0362f09e97bc264
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-02-12 00:03:42 +0800

    Calling the AuthPage and remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:social_media_clone/features/auth/presentation/pages/login_page.dart';
+import 'package:social_media_clone/features/auth/presentation/pages/auth_page.dart';
 import 'package:social_media_clone/firebase_options.dart';
 import 'package:social_media_clone/themes/light_mode.dart';
 
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightMode,
-      home: const LoginPage(),
+      home: const AuthPage(),
     );
   }
 }
