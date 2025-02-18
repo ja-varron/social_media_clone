d6c6d37ccca41b557a9fde63d2aa1d0c54034bfd
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-02-18 11:01:28 +0800

    editing static  profile pic into replacable picture via CachedNetworkImage

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_media_clone/features/profile/presentation/components/bio_box.dart';
@@ -58,7 +59,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       builder: (context) => EditProfilePage(user: user,),
                     )
                   ), 
-                  icon: Icon(Icons.settings)
+                  icon: const Icon(Icons.settings)
                 ),
               ],
             ),
@@ -79,21 +80,38 @@ class _ProfilePageState extends State<ProfilePage> {
                 const SizedBox(height: 25,),
                       
                 // profile pic
-                Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.secondary,
-                    borderRadius: BorderRadius.circular(12)
-                  ),
-                  height: 120,
-                  width: 120,
-                  padding: const EdgeInsets.all(25),
-                  child: Center(
-                    child: Icon(
-                      Icons.person,
+                CachedNetworkImage(
+                  imageUrl: user.profileImageUrl,
+                  // loading..
+                  placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+
+                  // error -> failed to load
+                  errorWidget: (context, url, error) => 
+                    Icon(
+                    Icons.person,
                       size: 72,
                       color: Theme.of(context).colorScheme.primary,
+
                     ),
-                  ),
+
+                  // loaded
+                  imageBuilder: (context, imageProvider) =>
+                    Container(
+                      height: 120,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover
+                        )
+                      ),
+                      child: Image(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                    )
                 ),
 
                 const SizedBox(height: 25,),
