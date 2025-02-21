6d362d02cf971578f482d3201fcd029e7ad836d7
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-02-21 09:39:52 +0800

    creating posts methods and its states

---
+++ lib/features/post/presentation/cubits/post_cubit.dart
@@ -0,0 +1,62 @@
+import 'dart:typed_data';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_clone/features/post/domain/entities/post.dart';
+import 'package:social_media_clone/features/post/domain/repos/post_repo.dart';
+import 'package:social_media_clone/features/post/presentation/cubits/post_states.dart';
+import 'package:social_media_clone/features/storage/domain/storage_repo.dart';
+
+class PostCubit extends Cubit<PostState> {
+  final PostRepo postRepo;
+  final StorageRepo storageRepo;
+
+  PostCubit({
+    required this.postRepo,
+    required this.storageRepo
+  }) : super(PostsInitial());
+
+  // create a new post
+  Future<void> createPost(Post post, {String? imagePath, Uint8List? imageBytes}) async {
+    String? imageUrl;
+
+    try {
+      // handle image upload for mobile platforms (using file path)
+      if(imagePath != null) {
+        emit(PostsUploading());
+        imageUrl = await storageRepo.uploadProfileImageMobile(imagePath, post.id);
+      }
+
+      // handle image upload for web platforms (using file bytes)
+      else if(imageBytes != null) {
+        emit(PostsUploading());
+        imageUrl = await storageRepo.uploadProfileImageWeb(imageBytes, post.id);
+      }
+
+      // give image url to post
+      final  newPost = post.copyWith(imageUrl: imageUrl);
+
+      // create post in the backend
+      postRepo.createPost(newPost);
+    } catch(e) {
+      emit(PostsError("Failed to create post: $e"));
+    }
+  }
+
+  // fetch all post
+  Future<void> fetchAllPosts() async {
+    try {
+      emit(PostsLoading());
+      final posts = await postRepo.fetchAllPost();
+      emit(PostsLoaded(posts));
+    } catch(e) {
+      emit(PostsError("Failed to fetch posts: $e"));
+    }
+  }
+
+  // delete a post
+  Future<void> deletePost(String postId) async {
+    try {
+      await postRepo.deletePost(postId);
+    } catch(e) {}
+  }
+}
---
+++ lib/features/post/presentation/cubits/post_states.dart
@@ -0,0 +1,38 @@
+/*
+
+POST STATES
+
+*/  
+
+import 'package:social_media_clone/features/post/domain/entities/post.dart';
+
+abstract class PostState {
+
+}
+
+// initial 
+class PostsInitial extends PostState {
+
+}
+
+// loading..
+class PostsLoading extends PostState {
+
+}
+
+// uploading
+class PostsUploading extends PostState {
+
+}
+
+// error
+class PostsError extends PostState {
+  final String message;
+  PostsError(this.message);
+}
+
+// loaded
+class PostsLoaded extends PostState {
+  final List<Post> posts;
+  PostsLoaded(this.posts);
+}
